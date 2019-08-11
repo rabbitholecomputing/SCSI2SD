dc2f3da785d5e6bd20b015dc4056966804afd201
Gregory Casamento <greg.casamento@gmail.com>
2019-08-11 14:48:13 -0400

    minor changes to script

--- software/build.sh
+++ software/build.sh
@@ -4,25 +4,19 @@ case `uname -s` in
 Linux)
 	# Builds all of the utilities (not firmware) under Linux.
 	# Requires mingw installed to cross-compile Windows targets.
-
 	(cd scsi2sd-util && ./build.sh)
-
 	if [ $? -eq 0 ]; then
 		mkdir -p build/linux
 		mkdir -p build/windows/64bit
 		mkdir -p build/windows/32bit
-
 		cp scsi2sd-util/build/linux/scsi2sd-util build/linux
-
 		cp scsi2sd-util/build/windows/32bit/scsi2sd-util.exe build/windows/32bit
-
 		cp scsi2sd-util/build/windows/64bit/scsi2sd-util.exe build/windows/64bit
 	fi
 ;;
 
 Darwin)
 	make -C scsi2sd-util
-
 	if [ $? -eq 0 ]; then
 		mkdir -p build/mac
 		cp scsi2sd-util/build/mac/scsi2sd-util build/mac
@@ -31,7 +25,7 @@ Darwin)
 
 MINGW32_NT-6.2)
 	make -C scsi2sd-util
-	if [ $? -eq 0]; then
+	if [ $? -eq 0 ]; then
 		mkdir -p build/win
 		cp scsi2sd-util/build/win/scsi2sd-util build/win
 	fi

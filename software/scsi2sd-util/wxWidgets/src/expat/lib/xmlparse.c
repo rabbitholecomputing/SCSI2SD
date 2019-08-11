b35dca96f3dfaf74c43d1391dcff1cb08bd9b20e
Gregory Casamento <greg.casamento@gmail.com>
2019-08-11 16:35:32 -0400

    Fix issue with xpat, use memcpy instead of memmove

--- software/scsi2sd-util/wxWidgets/src/expat/lib/xmlparse.c
+++ software/scsi2sd-util/wxWidgets/src/expat/lib/xmlparse.c
@@ -90,7 +90,7 @@ typedef char ICHAR;
 #ifdef HAVE_BCOPY
 #define memmove(d,s,l) bcopy((s),(d),(l))
 #else
-#define memmove(d,s,a) memcpy((d),(s),(l))
+#define memmove(d,s,a) memcpy((d),(s),(a))
 // #error memmove does not exist on this platform, nor is a substitute available
 #endif /* HAVE_BCOPY */
 #endif /* HAVE_MEMMOVE */

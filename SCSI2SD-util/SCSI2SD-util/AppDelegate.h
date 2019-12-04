f42eabbc420544196d96f798cb6760a41833d83d
Gregory Casamento <greg.casamento@gmail.com>
2019-12-03 20:31:02 -0500

    Add initial xcode project with new XIB file GUI from previous work

---
+++ SCSI2SD-util/SCSI2SD-util/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SCSI2SD-util
+//
+//  Created by Gregory Casamento on 12/3/19.
+//  Copyright © 2019 RabbitHole Computing, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ SCSI2SD-util/SCSI2SD-util/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  SCSI2SD-util
+//
+//  Created by Gregory Casamento on 12/3/19.
+//  Copyright © 2019 RabbitHole Computing, LLC. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end

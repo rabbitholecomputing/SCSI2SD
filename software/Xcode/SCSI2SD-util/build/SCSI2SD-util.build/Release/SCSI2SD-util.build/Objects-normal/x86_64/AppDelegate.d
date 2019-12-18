e5b62fab3a3f01f8a30017408c5ec749ee8afe11
Gregory Casamento <greg.casamento@gmail.com>
2019-12-18 15:49:17 -0500

    Use included hid code

--- software/Xcode/SCSI2SD-util/build/SCSI2SD-util.build/Release/SCSI2SD-util.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/heron/Development/SCSI2SD/software/Xcode/SCSI2SD-util/SCSI2SD-util/AppDelegate.m \
-  /Users/heron/Development/SCSI2SD/software/Xcode/SCSI2SD-util/SCSI2SD-util/AppDelegate.h
